1fbd8e02678e3d0c60648b175bf6f4e5906b83aa
christianmoraless <christiandmoraless@gmail.com>
2023-04-24 11:25:18 -0400

    feat:creating pokemons

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/pokemon-api'),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
